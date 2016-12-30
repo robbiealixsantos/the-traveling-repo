c6bd06cef3bfe79bbcd2150381841c83916ae39c
ffsommers <ssommers21@gmail.com>
2016-12-30 14:44:25 -0800

    adds collison and score to flappy.js

--- app/assets/javascripts/flappy.js
+++ app/assets/javascripts/flappy.js
@@ -19,10 +19,13 @@
 	        var spaceKey = game.input.keyboard.addKey(
                     Phaser.Keyboard.SPACEBAR);
             spaceKey.onDown.add(this.jump, this);
-             this.pipes = game.add.group();
-            
+             this.pipes = game.add.group()
 		   
            this.timer = game.time.events.loop(1500, this.addRowOfPipes, this); 
+	       this.score = 0;
+           this.labelScore = game.add.text(20, 20, "0", 
+              { font: "30px Arial", fill: "#ffffff" });   
+
 	      },
 
 	       addOnePipe: function(x, y) {
@@ -44,6 +47,8 @@
 		   },
 
 		    addRowOfPipes: function() {
+		       this.score += 1;
+			   this.labelScore.text = this.score; 
 		    // Randomly pick a number between 1 and 5
 		    // This will be the hole position
 		    var hole = Math.floor(Math.random() * 5) + 1;
@@ -59,6 +64,8 @@
 	    update: function() {
 	        if (this.rob.y < 0 || this.rob.y > 490)
                this.restartGame();
+             game.physics.arcade.overlap(
+    		this.rob, this.pipes, this.restartGame, null, this);    
            },
            
            jump: function() {
