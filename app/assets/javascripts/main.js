db34108c75ab7d8082e3f056a304febce1725883
ffsommers <ssommers21@gmail.com>
2016-12-30 12:45:22 -0800

    adds basic styling to welcome page as well as animations.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,16 +3,22 @@ $( document ).on('turbolinks:load', function(){
     if($('body').is('.typing')){
       typingGame();
     }
-    introFace();
+    
   });
-
- 
- 
+  introFace();
 });
 
 
 var introFace = function() {
 	  $('.intro').addClass('animated bounceInLeft');
-	  $('.intro').addClass('animated swing');
+	 setTimeout(function(){
+	 	 $('.intro').removeClass('animated bounceInLeft');
+	 	$('.intro').addClass('animated rotateIn');
+	 	setTimeout(function(){
+	 	  $('.intro').removeClass('animated rotateIn');	 
+	 	  $('.intro').addClass('animated flipOutY');	
+	 	},1000) 
+	 	
+	 }, 1000); 
 
 }
